e0a96af61b4b5a3b15fde38d54110d0a6fdb97a0
codwerk <louis_widdy@live.fr>
2020-05-05 15:35:50 +0200

    start projet card vue

---
+++ cards-vue/src/App.vue
@@ -0,0 +1,113 @@
+<template lang="pug">
+  main#app
+    section.cards
+      .container
+        .joueur-list
+          Joueur(v-for="joueur in joueurs" :key="joueur.id" :joueur="joueur")
+
+</template>
+
+<script>
+
+import Joueur from './components/Joueur.vue';
+
+export default {
+  name: 'App',
+  components: { Joueur },
+  data () {
+    return {
+      joueurs: [
+        { 
+          libelle: 'lm',
+          nomComplet: 'L.Messi',
+          surnom: 'La pulga',
+          age: '32 ans',
+          salaire: '26 milions',
+          poste: 'Attaquant polyvalent',
+          buts : 627,
+          src: require('./assets/img/lm.png'),
+          club: require('./assets/img/barcelone.png')
+        },
+        { 
+          libelle: 'cr',
+          nomComplet: 'C.Ronaldo',
+          surnom: 'aucun',
+          age: '35 ans',
+          salaire: '31 millions',
+          poste: 'Attaquant',
+          buts: 626,
+          src: require('./assets/img/cr.png'),
+          club: require('./assets/img/juventus.png')
+        },
+        {
+          libelle: 'rl',
+          nomComplet: 'R.Lewandowski',
+          surnom: 'aucun',
+          age: '31 ans',
+          salaire: '15 millions',
+          poste: 'Avant-centre',
+          buts: 411,
+          src: require('./assets/img/rl.png'),
+          club: require('./assets/img/bayern.png')
+        },
+        { 
+          libelle: 'km',
+          nomComplet: 'K.Mbappé',
+          surnom: 'aucun',
+          age: '21 ans',
+          salaire: '17,5 millions',
+          poste: 'Attaquant',
+          buts: 117,
+          src: require('./assets/img/km.png'),
+          club: require('./assets/img/psg.png')
+        }
+      ]
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    
+    .cards {
+        height: 100vh;
+      
+
+      @media screen and (min-width: 640px) {
+        .container {
+        
+        max-width: 1000px;
+        margin: 0 auto;
+        padding: 0px 20px;
+        
+        .joueur-list {
+          display: flex;
+          flex-wrap: wrap;
+          flex-direction: row;
+          justify-content: space-between;
+          align-content: center;
+          height: 100vh;
+        }
+      }
+      }
+
+      @media screen and (max-width: 640px) {
+        .container {
+
+          .joueur-list  {
+
+            flex-direction: column;
+          }
+        }
+      }
+    }
+
+
+    
+  }
+</style>
---
+++ cards-vue/src/components/Joueur.vue
@@ -0,0 +1,123 @@
+<template lang="pug">
+    .joueur(:id="joueur.libelle")
+      img(:src="joueur.src")
+      .joueur-inner
+        
+        .joueur-name-wrap
+          h2.joueur-nC {{ joueur.nomComplet }}
+        .joueur-image-wrap
+          img(:src="joueur.club")
+        .joueur-details
+          h2 {{ joueur.buts }} {{ buts === 1 ? 'but' : 'buts'}}
+          p {{ joueur.age }}
+          p {{ joueur.salaire }}
+          p {{ joueur.poste }}
+</template>
+
+<script>
+export default {
+  name: 'Joueur',
+  props: {
+    joueur: [String, Number]
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+    .joueur {
+        
+        position: relative;
+        overflow: hidden;
+        cursor: pointer;
+        background-size: contain;
+        background-repeat: no-repeat;
+        transition: all 350ms ease-in-out;
+
+        &:hover {
+
+        }
+
+        /*&:nth-child(1) {
+          background-image: url('../assets/img/lm.png');
+        }
+
+        &:nth-child(2) {
+          background-image: url('../assets/img/cr.png');
+        }
+
+        &:nth-child(3) {
+          background-image: url('../assets/img/rl.png');
+        }
+
+        &:nth-child(4) {
+          background-image: url('../assets/img/km.png');
+        }*/
+          img {
+            width: 100%;
+            z-index: -1;
+            object-fit: cover;
+          }
+                  
+          .joueur-inner {
+            
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-40%, -10%);
+            width: 100%;
+            height: 100%;
+
+            &::before {
+                content: '';
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                width: 94%;
+                height: 95%;
+                background-image: linear-gradient(360deg, rgba(255, 255, 255, 0), rgb(205, 205, 205) 500%);
+                opacity: 0;
+                transition: all 300ms ease-in-out;
+                border-radius: 15px;
+            }
+
+            &:hover::before {
+              opacity: 1;
+            }
+
+            .joueur-name-wrap {
+              .joueur-nC  {
+                  margin: 0;
+                  padding: 0;
+                  color: #33d9b2;
+                  font-size: 20px;
+                  text-transform: capitalize;
+                  letter-spacing: 1px;
+
+              }
+            }
+
+            .joueur-image-wrap {
+              img {
+                width: 25%;
+              }
+            }
+
+            .joueur-details {
+
+            }
+          }
+
+          @media screen and (min-width: 640px) {
+
+            width: 25%;
+            //height: 300px;
+           }
+
+
+          @media screen and (max-width: 640px) {
+            width: 100%;
+          }
+    }
+</style>
